2d9da42847fe052b29544bbbea73e907ee8191eb
Patrick Corcoran <patrickcorcoran@Patricks-MacBook-Pro.local>
2021-02-10 09:25:36 -0600

    commit with port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const logger = require("morgan")
 const mongoose = require("mongoose")
-const PORT = 3000 || process.env.PORT
+const PORT = 3000
 
 const app = express()
 
